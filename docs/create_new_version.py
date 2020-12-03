750393651c56c933ba7e9a65a8486987bd7204cd
phseiff <31518703+phseiff@users.noreply.github.com>
2020-12-03 03:37:55 +0100

    Make sure that the download page is actually saved.

--- docs/create_new_version.py
+++ docs/create_new_version.py
@@ -6,7 +6,7 @@ in_file_name = "spec.tex"
 in_file_prefix = in_file_name.split(".")[0]
 out_file_name = in_file_name.split(".")[0] + ".html"
 
-with open(out_file_name, "w"):
+with open(out_file_name, "w") as f:
     try:
         former_content = requests.get("https://phseiff.com/gender-render/" + out_file_name).text
     except:
@@ -14,9 +14,10 @@ with open(out_file_name, "w"):
     content_of_tex_file = open(in_file_name, "r").read()
     version = content_of_tex_file.split("\\newcommand{\\version}{", 1)[-1].split("}", 1)[0]
     if not ">" + version + "<" in former_content:
-        former_content = former_content.replace(
+        finished_content = former_content.replace(
             "<!--new:" + in_file_prefix + "-->",
             "<!--new:" + in_file_prefix + "-->"
             + '<li><a href="https://github.com/phseiff/gender-render/raw/' + github_sha + "/docs/" + in_file_name + '">'
             + version + "</a></li>"
         )
+        f.write(finished_content)
