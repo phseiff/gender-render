5a2f9b2e2d60f03d2ac54d6679d5d84dc724ca7f
phseiff <31518703+phseiff@users.noreply.github.com>
2020-12-03 03:06:05 +0100

    Add builder for simple download page.

---
+++ docs/create_new_version.py
@@ -0,0 +1,19 @@
+import sys
+
+github_sha = sys.argv[1]
+in_file_name = "spec.tex"
+in_file_prefix = in_file_name.split(".")[0]
+out_file_name = in_file_name.split(".")[0] + ".html"
+
+with open(out_file_name, "w"):
+    former_content = open("spec.html", "r").read()
+    content_of_tex_file = open(in_file_name, "r").read()
+    version = content_of_tex_file.split("\\newcommand{\\version}{", 1)[-1].split("}", 1)[0]
+    if not ">" + version + "<" in former_content:
+        former_content = former_content.replace(
+            "<!--new:" + in_file_prefix + "-->",
+            "<!--new:" + in_file_prefix + "-->"
+            + '<li><a href="https://github.com/phseiff/gender-render/raw/' + github_sha + "/docs/" + in_file_name + '">'
+            + "</li>"
+            + version + "</a>"
+        )
