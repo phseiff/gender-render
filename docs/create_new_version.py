250c10254a31feb2faf98d3efda52c763b72bdd5
phseiff <31518703+phseiff@users.noreply.github.com>
2020-12-03 04:04:36 +0100

    Ensure the download page gets made even if there is no new spec version.

--- docs/create_new_version.py
+++ docs/create_new_version.py
@@ -15,10 +15,10 @@ with open(out_file_name, "w") as f:
     content_of_tex_file = open(in_file_name, "r").read()
     version = content_of_tex_file.split("\\newcommand{\\version}{", 1)[-1].split("}", 1)[0]
     if not ">" + version + "<" in former_content:
-        finished_content = former_content.replace(
+        former_content = former_content.replace(
             "<!--new:" + in_file_prefix + "-->",
             "<!--new:" + in_file_prefix + "-->"
             + '<li><a href="https://github.com/phseiff/gender-render/raw/' + github_sha + "/docs/" + in_file_prefix
             + '.pdf">' + version + "</a></li>"
         )
-        f.write(finished_content)
+    f.write(former_content)
